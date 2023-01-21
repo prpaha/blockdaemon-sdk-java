cb17e5d9f223327a6195357cdaa8ec9c662c3248
Павел Проскурин <Pavel.Proskurin@vsegda-da.com>
2023-01-21 22:00:09 +0300

    Add send data method

--- src/main/java/ru/prpaha/blockdaemon/TransactionsService.java
+++ src/main/java/ru/prpaha/blockdaemon/TransactionsService.java
@@ -1,6 +1,7 @@
 package ru.prpaha.blockdaemon;
 
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.prpaha.blockdaemon.dto.BlockInfo;
@@ -10,6 +11,7 @@ import ru.prpaha.blockdaemon.model.FeeEstimate;
 import ru.prpaha.blockdaemon.model.FeeEstimateEstimatedFees;
 import ru.prpaha.blockdaemon.model.TxOutputs;
 import ru.prpaha.blockdaemon.model.TxPage;
+import ru.prpaha.blockdaemon.model.TxReceipt;
 import ru.prpaha.blockdaemon.repository.AccountsRepository;
 import ru.prpaha.blockdaemon.repository.PlatformsRepository;
 import ru.prpaha.blockdaemon.repository.TransactionsRepository;
@@ -25,15 +27,19 @@ public class TransactionsService {
     @Value("${blockdaemon.network}")
     private String network;
 
-    public TxOutputs getTransactions(BlockdaemonPlatform platform, String address, String continuation) throws ApiException {
+    public TxOutputs getTransactions(@NotNull BlockdaemonPlatform platform,
+                                     @NotNull String address,
+                                     String continuation) throws ApiException {
         return accountsRepository.getTransactionsUtxo(platform.getValue(), network, address, continuation);
     }
 
-    public TxPage getTransactionsTxs(BlockdaemonPlatform platform, String address, String continuation) throws ApiException {
+    public TxPage getTransactionsTxs(@NotNull BlockdaemonPlatform platform,
+                                     @NotNull String address,
+                                     String continuation) throws ApiException {
         return accountsRepository.getTransactionsTxs(platform.getValue(), network, address, continuation);
     }
 
-    public FeeEstimate getFeeEstimates(BlockdaemonPlatform platform) throws ApiException {
+    public FeeEstimate getFeeEstimates(@NotNull BlockdaemonPlatform platform) throws ApiException {
         return switch (platform) {
             case ETHEREUM -> map(transactionsRepository.getEthereumFeeEstimates(platform.getValue(), network));
             default -> transactionsRepository.getFeeEstimates(platform.getValue(), network);
@@ -52,11 +58,16 @@ public class TransactionsService {
                         .slow(ethereumFeeEstimates.getEstimatedFees().getSlow().getMaxPriorityFee()));
     }
 
-    public BlockInfo getCurrentBlockInfo(BlockdaemonPlatform platform) throws ApiException {
+    public BlockInfo getCurrentBlockInfo(@NotNull BlockdaemonPlatform platform) throws ApiException {
         return BlockInfo.builder()
                 .id(platformsRepository.getCurrentBlockId(platform.getValue(), network))
                 .number(platformsRepository.getCurrentBlockNumber(platform.getValue(), network))
                 .build();
+    }
+
+    public TxReceipt sendSignedTransaction(@NotNull BlockdaemonPlatform platform,
+                                           @NotNull String signedTransactionHex) throws ApiException {
+        return transactionsRepository.sendSignedTransaction(platform.getValue(), network, signedTransactionHex);
     }
 
 }
--- src/main/java/ru/prpaha/blockdaemon/repository/TransactionsRepository.java
+++ src/main/java/ru/prpaha/blockdaemon/repository/TransactionsRepository.java
@@ -2,10 +2,13 @@ package ru.prpaha.blockdaemon.repository;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.jetbrains.annotations.NotNull;
 import ru.prpaha.blockdaemon.api.TransactionsApi;
 import ru.prpaha.blockdaemon.dto.EthereumFeeEstimate;
 import ru.prpaha.blockdaemon.invoker.ApiException;
 import ru.prpaha.blockdaemon.model.FeeEstimate;
+import ru.prpaha.blockdaemon.model.SignedTx;
+import ru.prpaha.blockdaemon.model.TxReceipt;
 
 public class TransactionsRepository extends AbstractApiRepository {
 
@@ -16,14 +19,22 @@ public class TransactionsRepository extends AbstractApiRepository {
         this.transactionsApi = transactionsApi;
     }
 
-    public FeeEstimate getFeeEstimates(String platform, String network) throws ApiException {
+    public FeeEstimate getFeeEstimates(@NotNull String platform,
+                                       @NotNull String network) throws ApiException {
         return transactionsApi.feeEstimate(platform, network);
     }
 
-    public EthereumFeeEstimate getEthereumFeeEstimates(String platform, String network) throws ApiException {
+    public EthereumFeeEstimate getEthereumFeeEstimates(@NotNull String platform,
+                                                       @NotNull String network) throws ApiException {
         var call = transactionsApi.feeEstimateCall(platform, network, null);
         var returnType = new TypeToken<EthereumFeeEstimate>(){}.getType();
         return (EthereumFeeEstimate) transactionsApi.getApiClient().execute(call, returnType).getData();
+    }
+
+    public TxReceipt sendSignedTransaction(@NotNull String platform,
+                                           @NotNull String network,
+                                           @NotNull String transaction) throws ApiException {
+        return transactionsApi.txSend(platform, network, new SignedTx().tx(transaction));
     }
 
 }
